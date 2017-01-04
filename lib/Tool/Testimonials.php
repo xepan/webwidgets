e189918317ad19e1834a2a9dc3127a17520abb0d
Vaibhav Sharma <vaibhav@vaibhav.local>
2017-01-04 16:02:13 +0530

    webwidget testimonial made

--- lib/Tool/Testimonials.php
+++ lib/Tool/Testimonials.php
@@ -1,29 +1,64 @@
 <?php
 
+# VAIBHAV_TODO
+# paginator not working
+# List grid responsive
+
 namespace xepan\webwidgets;
 
 class Tool_Testimonials extends \xepan\cms\View_Tool{
-	public $options = [
-				'record_limit'=>null,
-				'template_type' =>'corusel'
-			];
 	public $count = 1;
+	public $options = [
+				'template_type' =>'grid',
+				'record_limit'=>null,
+				'add_paginator'=>true,
+				'grid_paginator_limit'=>10,
+				'list_paginator_limit'=>10,
+				'list_column_width'=>4,
+				'show_created_at'=>true,
+				'show_title'=>true,
+			];
+
 	function init(){
 		parent::init();
 
-		$template = 'tool\testimonial/'.$this->options['template_type'];
 		$testimonial_m = $this->add('xepan\webwidgets\Model_Testimonial');
+		
+		if($record_limit = $this->options['record_limit'])
+			$testimonial_m->setLimit($record_limit);
+
+		$template = 'tool\testimonial/'.$this->options['template_type'];
 		$testimonial_cl = $this->add('CompleteLister',null,null,[$template]);
 		$testimonial_cl->setModel($testimonial_m);	
 		
-		if($this->options['template_type'] == 'corusel'){
-			$testimonial_cl->addHook('formatRow',function($l){
+		// if($this->options['template_type'] == 'grid' And $this->options['add_paginator'] == true){																													
+		// 	$testimonial_cl->add('Paginator',['ipp'=>$this->options['grid_paginator_limit']]);	
+		// }
+
+		// if($this->options['template_type'] == 'list' And $this->options['add_paginator'] == true){
+		// 	$testimonial_cl->add('Paginator',['ipp'=>$this->options['list_paginator_limit']]);	
+		// }
+
+		$testimonial_cl->addHook('formatRow',function($l){
+			if($this->options['template_type'] == 'corusel'){
 				if($this->count == 1)
 					$l->current_row_html['active'] = "active";
 				else
 					$l->current_row_html['active'] = "deactive";
 				$this->count++;
-			});
-		}
+			}
+
+			if($this->options['show_title'] ==false){
+				$l->current_row_html['title_wrapper'] = ' ';
+			}
+
+			if($this->options['show_created_at'] == false){
+				$l->current_row_html['created_at_wrapper'] = ' ';
+			}
+
+			if($this->options['template_type'] =='list'){
+				$l->current_row_html['list_column_width'] = $this->options['list_column_width'];
+			}
+		});
 	}	
 }
