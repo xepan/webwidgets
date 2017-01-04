1ce0d83cbbd0e086257976ee6c4f5b802d9f68fc
Vaibhav Sharma <vaibhav@vaibhav.local>
2017-01-04 13:29:11 +0530

    corusel template made for testimonial

--- lib/Tool/Testimonials.php
+++ lib/Tool/Testimonials.php
@@ -7,13 +7,23 @@ class Tool_Testimonials extends \xepan\cms\View_Tool{
 				'record_limit'=>null,
 				'template_type' =>'corusel'
 			];
-
+	public $count = 1;
 	function init(){
 		parent::init();
 
 		$template = 'tool\testimonial/'.$this->options['template_type'];
 		$testimonial_m = $this->add('xepan\webwidgets\Model_Testimonial');
-		$testimonial_cl = $this->add('CompleteLister',null,null,[$template]);	
+		$testimonial_cl = $this->add('CompleteLister',null,null,[$template]);
 		$testimonial_cl->setModel($testimonial_m);	
+		
+		if($this->options['template_type'] == 'corusel'){
+			$testimonial_cl->addHook('formatRow',function($l){
+				if($this->count == 1)
+					$l->current_row_html['active'] = "active";
+				else
+					$l->current_row_html['active'] = "deactive";
+				$this->count++;
+			});
+		}
 	}	
 }
